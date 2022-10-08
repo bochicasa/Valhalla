0dd33dae9b9e519489c1072c27e8c94b038ecbaa
SANTIAGO ALONSO CORTES <santicortes@gmail.com>
2022-10-08 15:34:31 +0200

    add routing

--- src/Web/WebSPA/ClientApp/src/app/app.component.ts
+++ src/Web/WebSPA/ClientApp/src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private titleService : Title, public router:Router){
+
+  }
+
+
+  ngOnInit(): void {
+    console.log('app on int');
+  }
+
+  public setTitle(newTitle: string)
+  {
+    this.titleService.setTitle('Valhalla');
+  }
 }
---
+++ src/Web/WebSPA/ClientApp/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes, RouterModule } from '@angular/router';
+import { CatalogComponent } from './catalog/catalog.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'catalog', pathMatch: 'full' },
+    { path: 'catalog', component: CatalogComponent },
+];
+
+export const routing = RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' });
